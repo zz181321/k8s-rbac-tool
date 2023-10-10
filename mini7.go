a2a6fdd62ab4f0d34c595978ee2ed525dfbc3d7d
z181321 <dyhalt@gmail.com>
2023-10-10 09:44:53 -0400

    sorting ok

--- mini7.go
+++ mini7.go
@@ -5,20 +5,20 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"text/tabwriter"
 	"strings"
+	"text/tabwriter"
 	"flag"
 	"sort"
 )
 
-type ClusterRole struct {
+type Role struct {
 	APIVersion string       `json:"apiVersion"`
 	Kind       string       `json:"kind"`
-	Metadata   ClusterMeta  `json:"metadata"`
-	Rules      []ClusterRule `json:"rules"`
+	Metadata   RoleMetadata `json:"metadata"`
+	Rules      []RoleRule   `json:"rules"`
 }
 
-type ClusterMeta struct {
+type RoleMetadata struct {
 	Annotations        map[string]string `json:"annotations"`
 	CreationTimestamp  string            `json:"creationTimestamp"`
 	Labels             map[string]string `json:"labels"`
@@ -27,110 +27,106 @@ type ClusterMeta struct {
 	UID                string            `json:"uid"`
 }
 
-type ClusterRule struct {
+type RoleRule struct {
 	APIGroups     []string `json:"apiGroups"`
 	ResourceNames []string `json:"resourceNames,omitempty"`
 	Resources     []string `json:"resources"`
 	Verbs         []string `json:"verbs"`
 }
 
-// structure for sorting
-type ByAPIGroup []ClusterRule
+// Structure for sorting rules by APIGroup
+type SortByAPIGroup []RoleRule
 
-func (a ByAPIGroup) Len() int { return len(a) }
-func (a ByAPIGroup) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a ByAPIGroup) Less(i, j int) bool {
+func (a SortByAPIGroup) Len() int           { return len(a) }
+func (a SortByAPIGroup) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a SortByAPIGroup) Less(i, j int) bool {
 	if len(a[i].APIGroups) == 0 {
 		return true
 	}
 	if len(a[j].APIGroups) == 0 {
 		return false
 	}
-	return a[i].APIGroups[0] < a[j].APIGroups[0] // 첫번째 apiGroup을 기준으로 정렬
+	return a[i].APIGroups[0] < a[j].APIGroups[0] 
 }
 
-
-//--nosys prefix filter
-func hasExcludedPrefix(s string, prefixes []string) bool {
+// Check if the role name has a default system prefix
+func isSystemRole(roleName string, prefixes []string) bool {
 	for _, prefix := range prefixes {
-		if strings.HasPrefix(s, prefix) {
+		if strings.HasPrefix(roleName, prefix) {
 			return true
 		}
 	}
 	return false
 }
 
-func printUsage() {
+func displayUsage() {
 	fmt.Println("Usage:")
 	fmt.Println("./rbac-tool --table clusterrole       Display cluster roles in a table format.")
 	fmt.Println("./rbac-tool --table clusterrole --nosys  Display cluster roles in a table format excluding default system Cluster Roles.")
 }
 
-
 func main() {
-	excludePrefixes := []string{"system:", "kubeadm:", "calico"} // Default System Roles
-	
+	systemPrefixes := []string{"system:", "kubeadm:", "calico"} 
 	var tableOption string
-	var nosys bool
-	flag.StringVar(&tableOption, "table", "", "Display cluster roles in a table format (use 'clusterrole')")
-	flag.BoolVar(&nosys, "nosys", false, "Exclude roles that default system Cluster Roles")
+	var excludeSystem bool
+	flag.StringVar(&tableOption, "table", "", "Display roles in a table format (use 'clusterrole')")
+	flag.BoolVar(&excludeSystem, "nosys", false, "Exclude default system Cluster Roles")
 	flag.Parse()
 
-	out, err := exec.Command("kubectl", "get", "clusterroles", "-o", "json").Output()
+	output, err := exec.Command("kubectl", "get", "clusterroles", "-o", "json").Output()
 	if err != nil {
 		panic(err)
 	}
 
 	if tableOption == "" {
-		printUsage()
+		displayUsage()
 		return
 	}
 
 	if tableOption != "clusterrole" {
-		fmt.Println("Invalid value for --table option. Expected 'clusterrole'.")
+		fmt.Println("Invalid table option. Expected 'clusterrole'.")
 		return
 	}
 
-
-var clusterRolesList struct {
-    Items []ClusterRole `json:"items"`
-}
-
-err = json.Unmarshal(out, &clusterRolesList)
-if err != nil {
-	panic(err)
-}
-
-for i := range clusterRoleList.Items {
-	sort.Srot(BYAPIGroup(clusterRoleList.Items[i].Rules))
-}
-
-
-w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
-fmt.Fprintln(w, "Kind\tRole (Name)\tapiGroups\tResources\tVerbs")
-fmt.Fprintln(w, "-----------\t----------\t---------\t---------\t-----")
-
-for _, role := range clusterRolesList.Items {
-	if nosys && hasExcludedPrefix(role.Metadata.Name, excludePrefixes) {
-	continue
+	var rolesList struct {
+	    Items []Role `json:"items"`
 	}
-    printedHeaderForRole := false // 헤더를 이미 출력했는지 체크
-    for _, rule := range role.Rules {
-        for _, apiGroup := range rule.APIGroups {
-            for _, resource := range rule.Resources {
-                if !printedHeaderForRole {
-                    fmt.Fprintf(w, "%s\t%s\t%s\t%s\t[%s]\n", role.Kind, role.Metadata.Name, apiGroup, resource, strings.Join(rule.Verbs, ", "))
-                    printedHeaderForRole = true
-                } else {
-                    fmt.Fprintf(w, "\t\t%s\t%s\t[%s]\n", apiGroup, resource, strings.Join(rule.Verbs, ", "))
-                }
-            }
-        }
-    }
-    if printedHeaderForRole { // 만약 규칙이 있으면 구분자 출력
-        fmt.Fprintln(w, "-----------\t----------\t---------\t---------\t-----")
-    }
+
+	err = json.Unmarshal(output, &rolesList)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := range rolesList.Items {
+		sort.Sort(SortByAPIGroup(rolesList.Items[i].Rules))
+	}
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
+	fmt.Fprintln(w, "Kind\tRole (Name)\tapiGroups\tResources\tVerbs")
+	fmt.Fprintln(w, "-----------\t---------\t----------\t---------\t------")
+
+	for _, role := range rolesList.Items {
+		if excludeSystem && isSystemRole(role.Metadata.Name, systemPrefixes) {
+			continue
+		}
+		displayedHeader := false
+		for _, rule := range role.Rules {
+			for _, apiGroup := range rule.APIGroups {
+				for _, resource := range rule.Resources {
+					if !displayedHeader {
+						fmt.Fprintf(w, "%s\t%s\t%s\t%s\t [%s]\n", role.Kind, role.Metadata.Name, apiGroup, resource, strings.Join(rule.Verbs, ", "))
+						displayedHeader = true
+					} else {
+						fmt.Fprintf(w, "\t\t%s\t%s\t [%s]\n", apiGroup, resource, strings.Join(rule.Verbs, ", "))
+					}
+				}
+			}
+		}
+		if displayedHeader {
+			fmt.Fprintln(w, "-----------\t---------\t----------\t---------\t------")
+		}
+	}
+
+	w.Flush()
 }
 
-w.Flush()
-}
